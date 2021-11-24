69a99c4e783ebd95acb058592db0cafc1cf4ed35
Ubuntu <ubuntu@ip-172-31-5-196.ec2.internal>
2021-11-24 03:48:22 +0000

    Created helloWorldUpdated.cpp and added two lines of code to it

---
+++ helloWorldUpdated.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+  std::cout << "Hello World!\n";
+  std::cout << "By: Melody berhane\n";
+  std::cout << "I am very happy to be in a programming class again.\n";
+  std::cout << "Hello Mom, I miss you so much!! See you when i get home.\n";
+  std::cout << "I can't wait till december 17th, i am so excited to go back home to Cameroon!\n";
+} 
